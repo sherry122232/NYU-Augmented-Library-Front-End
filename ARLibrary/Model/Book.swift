61e2100ff8d93ac7a24f96b8aaf4b09b93bb707c
Shuyang Wang <45084681+sherry122232@users.noreply.github.com>
2024-02-29 19:13:15 -0500

    Add files via upload

---
+++ ARLibrary/Model/Book.swift
@@ -0,0 +1,24 @@
+//
+//  Book.swift
+//  ARLibrary
+//
+//  Created by 王姝洋 on 12/4/23.
+//
+
+import SwiftUI
+
+let books = [
+    Book(name: "Paper",
+         imageUrl: "https://i.ibb.co/XVR1HqH/image-28.png",
+         author:"Yukio Mishima",
+         description:"this is the paper book's description"),
+    Book(name: "Lets (not) falling love", imageUrl: "https://cdn.gramedia.com/uploads/items/9786020520278_Lets-Not-Falling-In-Love.jpg",author:"Yukio Mishima",
+         description:"this is the paper book's description"),
+    Book(name: "Harga sebuah percaya", imageUrl: "https://cdn.gramedia.com/uploads/items/9786029474121C_9786029474121.jpg",
+         author:"Yukio Mishima",
+         description:"this is the paper book's description"),
+    Book(name: "Book 4", imageUrl: "https://i.ibb.co/XVR1HqH/image-28.png",author:"Yukio Mishima",
+         description:"this is the paper book's description"),
+    Book(name: "Book 5", imageUrl: "https://i.ibb.co/XVR1HqH/image-28.png",author:"Yukio Mishima",
+         description:"this is the paper book's description")
+]
